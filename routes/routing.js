3284ee232548e33ce048084938f2aa9dc1e13e97
Mayur Agarwal <mayuragarwal2812@gmail.com>
2023-07-02 13:55:25 +0530

    added fix

--- routes/routing.js
+++ routes/routing.js
@@ -203,7 +203,7 @@ routes.get(`/${process.env.USERS_ROUTE}`, async (req, res, next) => {
   try {
     if (req.query.key === process.env.KEY) {
       let data = await service.getUsersList();
-      res.json({ data: data }).status(200);
+      res.send(data).status(200);
     } else {
       res.json({ message: "Access Denied" }).status(403);
     }
